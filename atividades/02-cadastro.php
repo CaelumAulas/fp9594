0ff17e19e36561936aeb0672c8eb752f7a7b8dd5
Jhonatan Jacinto <jhonatanjacinto@gmail.com>
2021-04-21 23:09:06 -0300

    Aula 3

--- atividades/02-cadastro.php
+++ atividades/02-cadastro.php
@@ -1,6 +1,19 @@
 <?php 
 
-// insira sua lógica aqui
+ini_set('display_errors', 'Off');
+
+$conexao = mysqli_connect('localhost', 'root2', 'admin', 'caelum');
+if (mysqli_connect_errno()) {
+    die('Não foi possível se conectar à base de dados!');
+}
+
+$lista_contatos = array();
+$resultado = mysqli_query($conexao, "SELECT * FROM mensagens_contato");
+
+if ($resultado)
+{
+    $lista_contatos = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+}
 
 ?>
 <!DOCTYPE html>
@@ -17,19 +30,27 @@
     <div class="container py-5">
         <h2>Cadastro de E-mails</h2>
         <p>
-            Utilize o formulário abaixo para realizar o cadastro de e-mails para recebimento de newsletter:
+            Utilize o formulário abaixo para realizar o cadastro de mensagens de e-mails:
         </p>
 
         <!-- Exibir a mensagem de erro ou de sucesso aqui -->
 
         <form method="POST" action="">
             <div class="form-group">
-                <label for="nome">Nome:</label>
+                <label for="nome">Nome completo:</label>
                 <input type="text" name="nome" class="form-control" />
             </div>
             <div class="form-group">
                 <label for="email">E-mail:</label>
                 <input type="email" name="email" class="form-control" />
+            </div>
+            <div class="form-group">
+                <label for="assunto">Assunto:</label>
+                <input type="text" name="assunto" class="form-control" />
+            </div>
+            <div class="form-group">
+                <label for="mensagem">Mensagem:</label>
+                <textarea name="mensagem" class="form-control" rows="5"></textarea>
             </div>
             <div class="form-group">
                 <button class="btn btn-primary">
@@ -46,21 +67,26 @@
                     <th width="5%">#</th>
                     <th>Nome</th>
                     <th>E-mail</th>
+                    <th>Assunto</th>
+                    <th>Mensagem</th>
                     <th width="5%"></th>
                 </tr>
             </thead>
             <tbody>
-                
+                <?php foreach($lista_contatos as $contato) : ?>
                     <tr>
-                        <td>1</td>
-                        <td>Alberto</td>
-                        <td>alberto@gmail.com</td>
+                        <td><?= $contato['id'] ?></td>
+                        <td><?= $contato['nome_completo'] ?></td>
+                        <td><?= $contato['email'] ?></td>
+                        <td><?= $contato['assunto'] ?></td>
+                        <td><?= $contato['mensagem'] ?></td>
                         <td>
-                            <a href="02-cadastro.php?excluir=" class="btn btn-danger">
+                            <a href="02-cadastro.php" class="btn btn-danger">
                                 <i class="fas fa-trash-alt"></i>
                             </a>
                         </td>
                     </tr>
+                <?php endforeach; ?>
             </tbody>
         </table>
     </div>

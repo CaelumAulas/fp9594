a9e9d214a9870a0ae33054cbb27bb267737eecbe
Jhonatan Jacinto <jhonatanjacinto@gmail.com>
2021-04-30 23:15:46 -0300

    Envio de e-mail PHP

--- projeto/contato.php
+++ projeto/contato.php
@@ -2,6 +2,48 @@
 
 /** Configurações Gerais */
 require_once "src/config.php";
+use PHPMailer\PHPMailer\PHPMailer;
+
+try 
+{
+    if ($_POST)
+    {
+        $nome = filter_input(INPUT_POST, 'nome_completo', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES) ?: throw new Exception('Nome é obrigatório!');
+        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL) ?: throw new Exception('E-mail é obrigatório!');
+        $assunto = filter_input(INPUT_POST, 'assunto', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES) ?: throw new Exception('Assunto é obrigatório!');
+        $mensagem = filter_input(INPUT_POST, 'mensagem', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES) ?: throw new Exception('Mensagem é obrigatório!');
+        
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = "mail.jhonatanjacinto.dev";
+        $mail->SMTPAuth = true;
+        $mail->Username = "cursophp@jhonatanjacinto.dev";
+        $mail->Password = "Xcfe$#@34";
+        $mail->SMTPSecure = '';
+        $mail->SMTPAutoTLS = false;
+        $mail->Port = 587;
+        $mail->setFrom("no-reply@autocaelum.com.br", 'AutoCaelum');
+        $mail->addAddress('jhonatanjacinto@gmail.com');
+        $mail->Subject = $assunto;
+        $mail->Body = "
+            Nova mensagem enviada por um cliente da AutoCaelum:
+            Nome: $nome
+            E-mail: $email
+            Mensagem: 
+            $mensagem
+        ";
+
+        if (!$mail->send()) {
+            throw new Exception($mail->ErrorInfo);
+        }
+
+        set_app_mensagem('E-mail enviado com sucesso!');
+    }
+}
+catch(Exception $exc)
+{
+    set_app_mensagem($exc->getMessage(), 'erro');
+}
 
 $titulo_pagina = "Entre em Contato";
 $texto_destaque = "Use o formulário abaixo para entrar em contato conosco.";
@@ -11,6 +53,9 @@ require_once "src/includes/cabecalho.php";
 ?>
 <h1>Contato</h1>
 <hr>
+
+<?php show_app_mensagem(); ?>
+
 <form method="POST" action="" class="row mx-0">
     <div class="form-group col-md-6">
         <label>Nome completo:</label>
